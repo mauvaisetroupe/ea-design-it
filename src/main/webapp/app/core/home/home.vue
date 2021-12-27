0ec551464a8774449f11461fc5377ed08d0a977d
mauvaisetroupe <lionel.brol@gmail.com>
2021-12-27 15:19:25 +0100

    Add explanation on main concept. Close #4

--- src/main/webapp/app/core/home/home.vue
+++ src/main/webapp/app/core/home/home.vue
@@ -6,19 +6,59 @@
 
     <div class="col-md-9">
       <h1 class="display-4">Welcome to EA Design IT</h1>
-
-      <div v-if="authenticated">
-        <h1>Import Excel files</h1>
+      <div>
+        <h2>Introduction</h2>
+        <p>
+          This application is tool to model application architecture building diagrams to document applications and interactions between
+          applications (data flows).
+        </p>
+        <h2>Main concepts</h2>
         <ul>
-          <li>Import Excel <a href="/application-import-upload-file">applications</a> file</li>
-          <li>Import Excel <a href="/flow-import-upload-file">flows</a> file</li>
-          <li>Import Excel <a href="/event-import-upload-file">event</a> file</li>
+          <li>
+            A <a href="/landscape-view">Landscape</a> is an architecture diagram that represents a subset of
+            <a href="/application">Application</a>, and their Functional Flow
+          </li>
+          <li>
+            In the context of a Landscape, a <a href="/functional-flow">Functional Flow</a> represents a functional information exchange
+            between two applications
+          </li>
+          <li>
+            A Functional Flow is implemented through one or more <a href="/flow-interface">Interface</a> to transfer information between
+            source and target.
+          </li>
+          <li>
+            Interface is a "pipeline" between two applications and it's fully defined by a source and a target Application and a
+            <a href="/protocol">Protocol</a>
+          </li>
+          <li>
+            In the context of a Function Flow, exchange data through a specific Interface is implemented by a
+            <a href="/data-flow">Data Flow</a> (concretly, DataFlow is a File, an Event in a topic or API call of a swagger)
+          </li>
+          <li>
+            <a href="/data-flow-item">Data Flow Item</a> is used for model a more fined-grained data exchange (operation of swagger,
+            substructure in the File, event of a topic)
+          </li>
         </ul>
+        <p>
+          Please, refer to <a href="https://github.com/mauvaisetroupe/EADesignIt/tree/main/documentation/metamodel">documentation</a> for
+          more explanation
+        </p>
+      </div>
+      <div v-if="authenticated">
+        <h2>Import Excel files</h2>
+        <ul>
+          <li>Import Excel <a href="/application-import-upload-file">Applications</a> file</li>
+          <li>Import Excel <a href="/flow-import-upload-file">Functional Flows &amp; Interfaces </a> file</li>
+          <li>Import Excel <a href="/event-import-upload-file">Data Flows &amp; Dat Flow Items</a> file</li>
+        </ul>
+        <p>
+          Please, refer to <a href="https://github.com/mauvaisetroupe/EADesignIt/tree/main/documentation/excel-import">documentation</a> for
+          more explanation
+        </p>
         <div class="alert alert-success">
           <span v-if="username">You are logged in as user "{{ username }}"</span>
         </div>
       </div>
-
       <div class="alert alert-warning" v-if="!authenticated">
         <span>Please </span><a class="alert-link" v-on:click="openLogin()">sign in</a>.
       </div>
