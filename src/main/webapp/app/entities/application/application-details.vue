7ef2530622f2de11fb8247cd49f8a5d79dbf4a27
mauvaisetroupe <lionel.brol@gmail.com>
2023-03-02 17:47:58 +0100

    Remove unused code Flow is never loaded when getting interfaces from an application

--- src/main/webapp/app/entities/application/application-details.vue
+++ src/main/webapp/app/entities/application/application-details.vue
@@ -132,7 +132,6 @@
       <table class="table">
         <thead>
           <tr>
-            <th scope="row"><span>Flow</span></th>
             <th scope="row"><span>Interface</span></th>
             <th scope="row"><span>Source</span></th>
             <th scope="row"><span>Target</span></th>
@@ -142,14 +141,6 @@
         </thead>
         <tbody>
           <tr v-for="caption in interfaces" v-bind:key="caption.id">
-            <td>
-              <span v-for="(flows, i) in caption.functionalFlows" :key="flows.id"
-                >{{ i > 0 ? ', ' : '' }}
-                <router-link :title="flows.description" :to="{ name: 'FunctionalFlowView', params: { functionalFlowId: flows.id } }">{{
-                  flows.alias
-                }}</router-link>
-              </span>
-            </td>
             <td>
               <router-link :to="{ name: 'FlowInterfaceView', params: { flowInterfaceId: caption.id } }">{{ caption.alias }}</router-link>
             </td>
