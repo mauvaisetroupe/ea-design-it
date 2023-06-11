3de803ec5c93567213317bf01ae579c53d7fad89
mauvaisetroupe <lionel.brol@gmail.com>
2023-06-12 00:05:04 +0200

    showApplications="false"

--- src/main/webapp/app/entities/capability/capability-navigate.vue
+++ src/main/webapp/app/entities/capability/capability-navigate.vue
@@ -2,7 +2,13 @@
   <div>
     <h2 class="jh-entity-heading" data-cy="capabilitiesNavigateHeading"><span>Capability Map</span></h2>
     <br />
-    <CapabilityComponent :capability="capability" @retrieveCapability="retrieveCapability" :path="path" :nbLevel="nbLevel" />
+    <CapabilityComponent
+      :capability="capability"
+      @retrieveCapability="retrieveCapability"
+      :path="path"
+      :nbLevel="nbLevel"
+      :showApplications="false"
+    />
   </div>
 </template>
 
