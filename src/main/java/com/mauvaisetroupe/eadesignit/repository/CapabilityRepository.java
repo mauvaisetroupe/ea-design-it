0efe1a5b3c42af7eaf4b57befa976d65d6fa4d29
mauvaisetroupe <lionel.brol@gmail.com>
2023-06-15 21:36:25 +0200

    remove duplicated method

--- src/main/java/com/mauvaisetroupe/eadesignit/repository/CapabilityRepository.java
+++ src/main/java/com/mauvaisetroupe/eadesignit/repository/CapabilityRepository.java
@@ -41,30 +41,4 @@ public interface CapabilityRepository extends JpaRepository<Capability, Long> {
     // s3 l = 1
     // s4 l = 2
     Optional<Capability> findById(@Param("id") Long id);
-
-    @Query(
-        value = "select c from Capability c" +
-        " left join fetch c.subCapabilities s1 " +
-        " left join fetch s1.subCapabilities s2 " +
-        " left join fetch s2.subCapabilities s3 " +
-        " left join fetch s3.subCapabilities s4 " +
-        " left join fetch s4.subCapabilities s5 " +
-        " left join fetch s5.subCapabilities s6 " +
-        " left join fetch c.capabilityApplicationMappings  m1 " +
-        " left join fetch s1.capabilityApplicationMappings m2 " +
-        " left join fetch s2.capabilityApplicationMappings m3 " +
-        " left join fetch s3.capabilityApplicationMappings m4 " +
-        " left join fetch s4.capabilityApplicationMappings m5 " +
-        " left join fetch s5.capabilityApplicationMappings m6 " +
-        " left join fetch s6.capabilityApplicationMappings m7 " +
-        " where c.id=:id"
-    )
-    // c = ROOT level =-2
-    // s1 l = -1 (surdomain)
-    // s2 l = 0
-    // s3 l = 1
-    // s4 l = 2
-    // s5 l = 3
-    // s6 l = 4 does not exist
-    Optional<Capability> findByIdAllLevel(@Param("id") Long id);
 }
