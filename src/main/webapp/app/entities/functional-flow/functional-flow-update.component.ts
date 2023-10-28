88ce4e01731f7a166cd5978191d78c2510df882d
mauvaisetroupe <lionel.brol@gmail.com>
2023-10-28 12:21:53 +0200

    Vue3  - Fix FunctionFlow Edit (except save)

--- src/main/webapp/app/entities/functional-flow/functional-flow-update.component.ts
+++ src/main/webapp/app/entities/functional-flow/functional-flow-update.component.ts
@@ -1,4 +1,4 @@
-import { computed, defineComponent, inject, ref, type Ref } from 'vue';
+import { computed, defineComponent, getCurrentInstance, inject, onMounted, ref, type Ref } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 import { useVuelidate } from '@vuelidate/core';
 
@@ -9,31 +9,118 @@ import { useAlertService } from '@/shared/alert/alert.service';
 import OwnerService from '@/entities/owner/owner.service';
 import { type IOwner } from '@/shared/model/owner.model';
 import { type IFunctionalFlow, FunctionalFlow } from '@/shared/model/functional-flow.model';
+import type { IPlantumlFlowImport } from '@/shared/model/plantuml-flow-import.model';
+import LandscapeViewService from '@/entities/landscape-view/landscape-view.service';
+import type { ILandscapeView } from '@/shared/model/landscape-view.model';
+import ApplicationService from '../application/application.service';
+import { nextTick } from 'process';
+import type { bvToast } from 'bootstrap-vue';
+
+// @Component({
+//   validations,
+//   watch: {
+//     nbLines: 'onLinesChnaged',
+//     plantuml: 'plantumlChange',
+//   },
+// })
 
 export default defineComponent({
   compatConfig: { MODE: 3 },
   name: 'FunctionalFlowUpdate',
   setup() {
+    //@Inject('functionalFlowService') private functionalFlowService: () => FunctionalFlowService;
     const functionalFlowService = inject('functionalFlowService', () => new FunctionalFlowService());
+    //@Inject('alertService') private alertService: () => AlertService;
     const alertService = inject('alertService', () => useAlertService(), true);
 
+    //public functionalFlow: IFunctionalFlow = new FunctionalFlow();
     const functionalFlow: Ref<IFunctionalFlow> = ref(new FunctionalFlow());
 
+    //@Inject('ownerService') private ownerService: () => OwnerService;
     const ownerService = inject('ownerService', () => new OwnerService());
 
+    //public owners: IOwner[] = [];
     const owners: Ref<IOwner[]> = ref([]);
+
+    //@Inject('landscapeViewService') private landscapeViewService: () => LandscapeViewService;
+    const landscapeViewService = inject('landscapeViewService', () => new LandscapeViewService());
+
+    //public allLandscapes: ILandscapeView[] = [];
+    const allLandscapes: Ref<ILandscapeView[]> = ref([]);
+    //public selectedLandscape: ILandscapeView = {};
+    const selectedLandscape: Ref<ILandscapeView> = ref({});
+
+    //@Inject('applicationService') private applicationService: () => ApplicationService;
+    const applicationService = inject('applicationService', () => new ApplicationService());
+
+    //public applications: string[] = [];
+    const applications: Ref<string[]> = ref([]);
+
     const isSaving = ref(false);
     const currentLanguage = inject('currentLanguage', () => computed(() => navigator.language ?? 'en'), true);
+    const plantuml = ref('');
+    const plantUMLImage = ref('');
+    const isFetching = ref(false);
+    const importError = ref('');
+    const previewError = ref('');
+    const functionalFlowImport: Ref<IPlantumlFlowImport> = ref({});
+    const tabIndex = 1;
+    const landscapeGivenInParameter = ref(false);
 
     const route = useRoute();
     const router = useRouter();
 
+    // beforeRouteEnter(to, from, next) {
+    //   next(vm => {
+    //     if (to.params.functionalFlowId) {
+    //       vm.retrieveFunctionalFlow(to.params.functionalFlowId);
+    //     } else {
+    //       // for creation, go on first tab
+    //       vm.chooseTab(0);
+    //     }
+    //     if (typeof to.params.tabIndex !== 'undefined') {
+    //       vm.chooseTab(parseInt(to.params.tabIndex));
+    //     }
+    //     vm.initRelationships();
+    //   });
+    // }
+
+    function chooseTab(_tabIndex) {
+      if (_tabIndex === 0) {
+        // https://github.com/bootstrap-vue/bootstrap-vue/issues/2803
+        nextTick(() => {
+          tabIndex.valueOf = _tabIndex;
+        });
+      }
+    }
+
+    // created(): void {
+    //   this.currentLanguage = this.$store.getters.currentLanguage;
+    //   this.$store.watch(
+    //     () => this.$store.getters.currentLanguage,
+    //     () => {
+    //       this.currentLanguage = this.$store.getters.currentLanguage;
+    //     }
+    //   );
+    // }
+    function created(): void {}
+
     const previousState = () => router.go(-1);
 
+    ////////////////////////////////////////////////
+    // On load, retrieve
+    // - FunctionFlow,
+    // - plantUML source fron flowID
+    // - plantUML image
+    // - table with potential interfaces (should be the same than flow detail)
+    /////////////////////////////////////////////////
+
+    // STEP 1 - Retrieve FunctionalFlow
     const retrieveFunctionalFlow = async functionalFlowId => {
       try {
         const res = await functionalFlowService().find(functionalFlowId);
         functionalFlow.value = res;
+        getPlantUMLSourceFromFlowId(functionalFlowId);
       } catch (error) {
         alertService.showHttpError(error.response);
       }
@@ -41,6 +128,129 @@ export default defineComponent({
 
     if (route.params?.functionalFlowId) {
       retrieveFunctionalFlow(route.params.functionalFlowId);
+    }
+
+    // STEP 2 - Retrieve plantuml source from flow ID
+    function getPlantUMLSourceFromFlowId(functionalFlowId) {
+      functionalFlowService()
+        .getPlantUMLSource(functionalFlowId, true, true)
+        .then(
+          res => {
+            plantuml.value = res.data;
+            isFetching.value = false;
+            getPlantUMLImageFromString();
+          },
+          err => {
+            console.log(err);
+          },
+        );
+    }
+
+    // STEP 3 : Retrieve plantuml Image from plantuml source
+    function getPlantUMLImageFromString() {
+      functionalFlowService()
+        .getPlantUMLFromString(plantuml.value)
+        .then(
+          res => {
+            plantUMLImage.value = res.data;
+            plantumlModified.value = false;
+            isFetching.value = false;
+            importPlantuml();
+          },
+          err => {
+            console.log(err);
+            plantUMLImage.value = '';
+          },
+        );
+    }
+
+    // STEP 3 : Retrieve interface list from plantuml Source
+    function importPlantuml() {
+      functionalFlowService()
+        .importPlantuml(plantuml.value)
+        .then(
+          res => {
+            functionalFlowImport.value = res.data;
+            if (functionalFlowImport.value && functionalFlowImport.value.flowImportLines) {
+              functionalFlowImport.value.flowImportLines.forEach(step => {
+                if (step.selectedInterface) {
+                  step.interfaceAlias = step.selectedInterface.alias;
+                }
+              });
+            }
+            isFetching.value = false;
+            previewError.value = '';
+          },
+          err => {
+            plantUMLImage.value = '';
+            functionalFlowImport.value = { flowImportLines: [] };
+            previewError.value = err;
+            plantumlModified.value = false;
+          },
+        );
+    }
+
+    function changeInterface(flowimportLine) {
+      if (!flowimportLine.selectedInterface) {
+        flowimportLine.interfaceAlias = '';
+      } else {
+        flowimportLine.interfaceAlias = flowimportLine.selectedInterface.alias;
+      }
+    }
+
+    function tabChanged() {
+      if (plantumlModified.value) getPlantUMLImageFromString();
+    }
+
+    //////////////////////////////////////////////////
+    // SAVE
+    //////////////////////////////////////////////////
+
+    const creation = computed(() => {
+      return !functionalFlow.value.id;
+    });
+
+    const aliasesValid = computed(() => {
+      if (!functionalFlowImport.value || !functionalFlowImport.value.flowImportLines) return true;
+      return !functionalFlowImport.value.flowImportLines.some(step => !step.interfaceAlias || step.interfaceAlias === '');
+    });
+
+    function save(): void {
+      isSaving.value = true;
+      const instance = getCurrentInstance() as any;
+
+      functionalFlowImport.value.id = functionalFlow.value.id;
+      functionalFlowImport.value.alias = functionalFlow.value.alias;
+      functionalFlowImport.value.description = functionalFlow.value.description;
+      functionalFlowImport.value.comment = functionalFlow.value.comment;
+      functionalFlowImport.value.status = functionalFlow.value.status;
+      functionalFlowImport.value.documentationURL = functionalFlow.value.documentationURL;
+      functionalFlowImport.value.documentationURL2 = functionalFlow.value.documentationURL;
+      functionalFlowImport.value.startDate = functionalFlow.value.startDate;
+      functionalFlowImport.value.endDate = functionalFlow.value.endDate;
+      functionalFlowImport.value.owner = functionalFlow.value.owner;
+
+      functionalFlowService()
+        .saveImport(functionalFlowImport.value, selectedLandscape.value.id)
+        .then(param => {
+          isSaving.value = false;
+          router.go(-1);
+          let message = 'A FunctionalFlow is created with identifier ' + param.id;
+          if (functionalFlow.value.id) {
+            message = 'A FunctionalFlow is updated with identifier ' + param.id;
+          }
+          instance.root.$bvToast.toast(message.toString(), {
+            toaster: 'b-toaster-top-center',
+            title: 'Info',
+            variant: 'info',
+            solid: true,
+            autoHideDelay: 5000,
+          });
+        })
+        .catch(error => {
+          isSaving.value = false;
+          alertService.showHttpError(error.response);
+        });
     }
 
     const initRelationships = () => {
@@ -52,6 +262,138 @@ export default defineComponent({
     };
 
     initRelationships();
+
+    ///////////////////////////////////////////////
+    // appliction autocmplete
+    ///////////////////////////////////////////////
+
+    const plantumlModified = ref(false);
+
+    const nbLines = computed(() => {
+      return plantuml.value.split(/\r\n|\r|\n/).length;
+    });
+
+    const lastLine = computed(() => {
+      return plantuml.value.split(/\r\n|\r|\n/).slice(-1)[0];
+    });
+
+    function onLinesChnaged() {
+      getPlantUMLImageFromString();
+    }
+
+    function plantumlChange() {
+      plantumlModified.value = true;
+
+      //console.log(this.inputSplitted);
+      selectedIndex.value = 0;
+      wordIndex.value = inputSplitted.value.length - 1;
+      focus();
+    }
+
+    const wordIndex = ref(0);
+    const selectedIndex = ref(0);
+    const searchMatch: Ref<string[]> = ref([]);
+    const clickedChooseItem = ref(false);
+
+    const listToSearch = computed(() => {
+      const standardItems = applications.value;
+      return standardItems;
+    });
+
+    const currentWord = computed(() => {
+      return plantuml.value.replace(/(\r\n|\n|\r)/gm, ' ').split(' ')[wordIndex.value];
+    });
+
+    const inputSplitted = computed(() => {
+      return plantuml.value.replace(/(\r\n|\n|\r)/gm, ' ').split(' ');
+    });
+
+    function highlightWord(word) {
+      const regex = new RegExp('(' + currentWord.value + ')', 'g');
+      return word.replace(regex, '<mark>$1</mark>');
+    }
+    function setWord(word) {
+      const currentWords = plantuml.value.replace(/(\r\n|\n|\r)/gm, '__br__ ').split(' ');
+      currentWords[wordIndex.value] = currentWords[wordIndex.value].replace(currentWord.value, '"' + word + '" ');
+      wordIndex.value += 1;
+      plantuml.value = currentWords.join(' ').replace(/__br__\s/g, '\n');
+    }
+    function moveDown() {
+      if (selectedIndex.value < searchMatch.value.length - 1) {
+        selectedIndex.value++;
+      }
+    }
+    function moveUp() {
+      if (selectedIndex.value !== -1) {
+        selectedIndex.value--;
+      }
+    }
+    function selectItem(index) {
+      selectedIndex.value = index;
+      chooseItem();
+    }
+    function chooseItem(e = null) {
+      console.log(e);
+      clickedChooseItem.value = true;
+
+      if (selectedIndex.value !== -1 && searchMatch.value.length > 0) {
+        if (e) {
+          e.preventDefault();
+        }
+        setWord(searchMatch.value[selectedIndex.value]);
+        selectedIndex.value = -1;
+      }
+    }
+    function focusout(e) {
+      setTimeout(() => {
+        if (!clickedChooseItem.value) {
+          searchMatch.value = [];
+          selectedIndex.value = -1;
+        }
+        clickedChooseItem.value = false;
+      }, 100);
+    }
+    function focus() {
+      searchMatch.value = [];
+      if (lastLine.value.includes(':')) {
+        searchMatch.value = [];
+      } else if (!currentWord.value || currentWord.value === '') {
+        console.log(currentWord.value);
+        searchMatch.value = [];
+      } else if (currentWord.value.length > 2) {
+        console.log(currentWord.value);
+        searchMatch.value = listToSearch.value.filter(el => el.toLowerCase().indexOf(currentWord.value.toLowerCase()) >= 0);
+      } else if (searchMatch.value.length === 1 && currentWord.value === searchMatch.value[0]) {
+        searchMatch.value = [];
+      }
+    }
+
+    const textareaNbLine = computed(() => {
+      if (functionalFlow.value.steps) {
+        let nb = functionalFlow.value.steps.length;
+        const distinvtID: Set<number> = new Set(
+          functionalFlow.value.steps
+            .map(step => step.group)
+            .filter(group => group) // to filter null values
+            .map(group => group.id),
+        );
+        const nbGroups = distinvtID.size;
+        nb = nb + 2 * nbGroups;
+        const margin = 2;
+        return nb + margin;
+      }
+      return 10;
+    });
+
+    onMounted(() => {
+      applicationService()
+        .retrieve()
+        .then(res => {
+          if (res.data) {
+            applications.value = res.data.map(appli => appli.name);
+          }
+        });
+    });
 
     const validations = useValidation();
     const validationRules = {
@@ -88,37 +430,31 @@ export default defineComponent({
       currentLanguage,
       owners,
       v$,
+      tabChanged,
+      tabIndex,
+      textareaNbLine,
+      plantuml,
+      focusout,
+      focus,
+      chooseItem,
+      moveDown,
+      moveUp,
+      searchMatch,
+      selectedIndex,
+      selectItem,
+      highlightWord,
+      getPlantUMLImageFromString,
+      plantumlModified,
+      plantUMLImage,
+      previewError,
+      functionalFlowImport,
+      changeInterface,
+      creation,
+      selectedLandscape,
+      allLandscapes,
+      landscapeGivenInParameter,
+      save,
+      aliasesValid,
     };
-  },
-  created(): void {},
-  methods: {
-    save(): void {
-      this.isSaving = true;
-      if (this.functionalFlow.id) {
-        this.functionalFlowService()
-          .update(this.functionalFlow)
-          .then(param => {
-            this.isSaving = false;
-            this.previousState();
-            this.alertService.showInfo('A FunctionalFlow is updated with identifier ' + param.id);
-          })
-          .catch(error => {
-            this.isSaving = false;
-            this.alertService.showHttpError(error.response);
-          });
-      } else {
-        this.functionalFlowService()
-          .create(this.functionalFlow)
-          .then(param => {
-            this.isSaving = false;
-            this.previousState();
-            this.alertService.showSuccess('A FunctionalFlow is created with identifier ' + param.id);
-          })
-          .catch(error => {
-            this.isSaving = false;
-            this.alertService.showHttpError(error.response);
-          });
-      }
-    },
   },
 });
